14ad6ed8d56d1e05e7203aafbdc93d78e987a1a9
ArkadiuszGwarda <45336756+ArkadiuszGwarda@users.noreply.github.com>
2018-11-27 12:18:24 +0100

    Nowka sztuka

---
+++ app/src/main/java/com/example/arkadio/myapplication/Testowa.java
@@ -0,0 +1,13 @@
+package com.example.arkadio.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Testowa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testowa);
+    }
+}
